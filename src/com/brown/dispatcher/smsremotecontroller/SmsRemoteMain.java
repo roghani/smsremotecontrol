6c7e8db72f2f5e363061a2d260612c5d78e28482
luis.gvalle <luis.gvalle@315aa3e3-6421-3f91-abc6-03316abcfc0e>
2010-08-08 13:42:11 +0000

    Interface rebuild: + Spinner + Textview with hint + Menu

--- src/com/brown/dispatcher/smsremotecontroller/SmsRemoteMain.java
+++ src/com/brown/dispatcher/smsremotecontroller/SmsRemoteMain.java
@@ -2,17 +2,32 @@ package com.brown.dispatcher.smsremotecontroller;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class SmsRemoteMain extends Activity implements OnClickListener, SmsRemoteCommon {
 	private static SharedPreferences mSettings;
+	ArrayList<String> appPackageArray;
+	private ArrayList<String> appNameArray;
 	
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,47 +36,135 @@ public class SmsRemoteMain extends Activity implements OnClickListener, SmsRemot
 		//Load preferences
 		mSettings = getSharedPreferences(PREFS_NAME, 0);
 		
-		//Register view listeners
-		ArrayList<Integer> views = new ArrayList<Integer>();
-		views.add(R.id.keyword_label);
-		views.add(R.id.keyword);
-		views.add(R.id.activity_label);
-		views.add(R.id.activityName);
-		views.add(R.id.exit);
-		views.add(R.id.tryit);
-		registerListeners(views);
+		//Fill spinner
+		Spinner s = (Spinner) findViewById(R.id.spinner);
+		ArrayAdapter<String> adapter = fillActivities();
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    s.setAdapter(adapter);
+	    s.setOnItemSelectedListener(new ActivitiesSpinnerListener());
 		
-		//Set classname & keyword info
-		fillData();
+		//Register view listeners
+		TextView keyword = (TextView)findViewById(R.id.keyword);
+		keyword.setText(mSettings.getString(KEYWORD, null));
+		keyword.addTextChangedListener(new KeywordWatcherListener());
+		
+	}
+	
+	private class KeywordWatcherListener implements TextWatcher {
 
+		@Override
+		public void afterTextChanged(Editable s) {
+			String keyword = s.toString();
+			SharedPreferences.Editor editor = mSettings.edit();
+			editor.putString(KEYWORD, keyword.equals("")?null:keyword);
+			editor.commit();
+			
+		}
+
+		@Override
+		public void beforeTextChanged(CharSequence s, int start, int count,
+				int after) {
+				//Do nothing
+		}
+
+		@Override
+		public void onTextChanged(CharSequence s, int start, int before,
+				int count) {
+				//Do nothing
+		}
+		
+	}
+	
+	private class ActivitiesSpinnerListener implements OnItemSelectedListener {
+
+	    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+	    	// Store package and class names in SharedPreferences
+	    	String className = (String) appNameArray.get(pos);
+			String packageName = (String) appPackageArray.get(pos);
+			SharedPreferences.Editor editor = mSettings.edit();
+			editor.putString(CLASSNAME, className);
+			editor.putString(PACKAGENAME, packageName);
+			editor.commit();
+	    }
+
+	    public void onNothingSelected(AdapterView parent) {
+	      // Do nothing.
+	    }
+	}
+	
+	
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.exit:
+	    	if (validateForm()) {
+	    		finish();
+	    	}
+	    	
+	        return true;
+	    case R.id.tryit:
+	    	if (validateForm()) {
+	    		tryit();
+	    	}
+	        
+	        return true;
+	    case R.id.about:
+	    	return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	private boolean validateForm() {
+		if (mSettings.getString(CLASSNAME, null) == null ||
+				mSettings.getString(KEYWORD, null) == null) {
+			Toast.makeText(this, R.string.validationFail, Toast.LENGTH_LONG).show();
+			return false;
+		}
+		return true;
 	}
 
-	private void registerListeners(ArrayList<Integer> views) {
-		Iterator<Integer> it = views.iterator();
+	/** 
+	 * Fills activities adapter to populate the spinner
+	 * @return
+	 */
+	private ArrayAdapter<String> fillActivities() {
+
+		// Get ACTION_MAIN applications
+		final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+		final List<ResolveInfo> pkgAppsList = getPackageManager().queryIntentActivities(mainIntent, 0);
+
+		// We need an array with package and class name of each activity
+		appPackageArray = new ArrayList<String>();
+		appNameArray = new ArrayList<String>();
+		
+		// Iterate over application list from PackageManager and get class and package names
+		Iterator<ResolveInfo> it = pkgAppsList.iterator();
 		while (it.hasNext()) {
-			View view = findViewById((Integer)it.next());
-			view.setOnClickListener(this);
+			ActivityInfo activityInfo = ((ResolveInfo) it.next()).activityInfo;
+
+			String activityPackgeName = activityInfo.name.substring(0, activityInfo.name.lastIndexOf("."));
+			String activityClassName = activityInfo.name.substring(activityInfo.name.lastIndexOf(".") + 1, activityInfo.name.length());
+			
+			appPackageArray.add(activityPackgeName);
+			appNameArray.add(activityClassName);
+			it.next();
 		}
+
+		// Build list
+		ArrayAdapter<String> clasess = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, appNameArray);
+		return clasess;
+
 	}
 
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.activity_label: case R.id.activityName: 
-			startActivityForResult(new Intent (this, SmsRemoteAdmin.class), 0);
-			break;
-		case R.id.keyword_label: case R.id.keyword:
-			startActivityForResult(new Intent (this, SmsRemoteKeywordEdit.class), 0);
-			break;
-		case R.id.tryit:
-			tryit();
-			break;	
-		case R.id.exit:
-			finish();
-			break;
-		default:
-			break;
-		}
-	}
+
 	
 	
 	/**
@@ -75,25 +178,9 @@ public class SmsRemoteMain extends Activity implements OnClickListener, SmsRemot
 	}
 		
 	
-	/** 
-	 * Every time we came back the view needs to be filled with new data.	
-	 */
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        fillData();
-    }
-	
-	/**
-	 * Populate main view form.
-	 * This is called everytime view needs to be reloaded.
-	 * Data is obtained from SharedPreferences.
-	 */
-	private void fillData() {
-		TextView classname = (TextView)findViewById(R.id.activityName);
-		classname.setText(mSettings.getString(CLASSNAME, null));
-		
-		TextView keywordV = (TextView)findViewById(R.id.keyword);
-		keywordV.setText(mSettings.getString(KEYWORD, null));
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
 		
 	}
 }
