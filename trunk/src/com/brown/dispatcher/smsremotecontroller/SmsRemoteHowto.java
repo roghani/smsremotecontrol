85dde2c7e0c99b2f46e0895d1f6d8a5c62230f1c
luis.gvalle <luis.gvalle@315aa3e3-6421-3f91-abc6-03316abcfc0e>
2010-08-08 17:17:48 +0000

    + Interface improvements. + Icons + Alerts + Text messages
    - Deleted unused classes.

---
+++ trunk/src/com/brown/dispatcher/smsremotecontroller/SmsRemoteHowto.java
@@ -0,0 +1,19 @@
+package com.brown.dispatcher.smsremotecontroller;
+/**
+ * @author lgzvalle@gmail.com
+ * Project site: https://code.google.com/p/smsremotecontrol/
+ * Code license: Apache 2.0
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SmsRemoteHowto extends Activity {
+	
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.howto);
+		
+	}
+	
+	
+}
--- trunk/src/com/brown/dispatcher/smsremotecontroller/SmsRemoteReceiver.java
+++ trunk/src/com/brown/dispatcher/smsremotecontroller/SmsRemoteReceiver.java
@@ -1,5 +1,10 @@
 package com.brown.dispatcher.smsremotecontroller;
 
+/**
+ * @author lgzvalle@gmail.com
+ * Project site: https://code.google.com/p/smsremotecontrol/
+ * Code license: Apache 2.0
+ */
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,7 +14,6 @@ import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class SmsRemoteReceiver extends BroadcastReceiver implements SmsRemoteCommon {
-	private static final String TAG = "SmsRemoteReceiver";
 	private static SharedPreferences mSettings = null;
 
 	@Override
@@ -24,25 +28,25 @@ public class SmsRemoteReceiver extends BroadcastReceiver implements SmsRemoteCom
 		String action = intent.getAction();
 		
 		// Get real sms or dummy one to try the receiver
-		if (action.equals("SMS_RECEIVED")) {
+		if (action.equals(SMS_RECEIVED)) {
 			Object[] pdus = (Object[]) extras.get("pdus");
 			
-			// We are only interested in array first object (sms)
+			// Only interested in array first object (last sms received)
 			SmsMessage message = SmsMessage.createFromPdu((byte[]) pdus[0]);
 			smsBody = message.getMessageBody();
 			
-		} else if (action.equals("TRYIT")) {
-			smsBody = extras.getString("fakeSms");
+		} else if (action.equals(TRY_IT)) {
+			smsBody = extras.getString(DUMMY_SMS);
 		}
 
-		
+		//If the message body contains the secret keyword, then launch selected application and forward message body to it.
 		if (isKeywordInSms(smsBody)) {
 			String packageName = mSettings.getString(PACKAGENAME, null);
 			String className = mSettings.getString(CLASSNAME, null);
 			
 			Intent di = new Intent();
 			di.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			di.putExtra("smsBody", smsBody);
+			di.putExtra(SMS_BODY, smsBody);
 			di.setClassName(packageName, packageName + "." + className);
 			
 			try {
@@ -61,7 +65,7 @@ public class SmsRemoteReceiver extends BroadcastReceiver implements SmsRemoteCom
 
 
 	/**
-	 * Check user secret keyword in sms body
+	 * Check if user secret keyword is in sms body
 	 * 
 	 * @param smsBody
 	 * @return
@@ -69,7 +73,7 @@ public class SmsRemoteReceiver extends BroadcastReceiver implements SmsRemoteCom
 	
 	private boolean isKeywordInSms(String smsBody) {
 		String keyword  = mSettings.getString(KEYWORD, null);
-		return smsBody.contains(keyword);
+		return (keyword != null && smsBody.contains(keyword));
 	}
 
 }
